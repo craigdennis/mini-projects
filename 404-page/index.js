89ee67991ef02b52bee1ae8863802ec5996bb895
Unknown <craigdennis1990@gmail.com>
2020-11-21 16:10:59 +0000

    having trouble in the neighbour hood
    got half way into it and having problems making the image appear

--- 404-page/index.js
+++ 404-page/index.js
@@ -13,31 +13,68 @@
       './img/santa-claus.png',
       './img/snowman.png',
     ];
-
-    let image = {};
-    let newImage = {};
-
+    let image = [];
+    let newImage = [];
     var total_number_of_images = the_images.length;
     var max_height = canvas.offsetHeight - icon_height;
     var max_width = canvas.offsetWidth - icon_width;
 
     var randomCoordinate = function () {
-      image.x = Math.floor(Math.random() * max_width);
-      image.y = Math.floor(Math.random() * max_height);
+      let x = Math.floor(Math.random() * max_width);
+      let y = Math.floor(Math.random() * max_height);
+      newImage.push([x, y]);
     };
-    var createImage = function (index, x, y) {
+    var createImage = function (index) {
       var node = icon_template.cloneNode(true);
       node.removeAttribute('id');
       node.removeAttribute('hidden');
-      node.style.top = y + 'px';
-      node.style.left = x + 'px';
+      node.style.top = newImage[i][1] + 'px';
+      node.style.left = newImage[i][0] + 'px';
       node.setAttribute('src', the_images[index]);
       canvas.appendChild(node);
     };
     for (var i = 0; i < total_number_of_images; i++) {
-      randomCoordinate()
-      createImage(i, image.x, image.y);
+      randomCoordinate();
+      createImage(i);
     }
+
+    const $bigBall = document.querySelector('.cursor__ball--big');
+    const $smallBall = document.querySelector('.cursor__ball--small');
+    const $hoverables = document.querySelectorAll('.hoverable');
+
+    // Listeners
+    document.body.addEventListener('mousemove', onMouseMove);
+    for (let i = 0; i < $hoverables.length; i++) {
+      $hoverables[i].addEventListener('mouseenter', onMouseHover);
+      $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+    }
+
+    // Move the cursor
+    function onMouseMove(e) {
+      TweenMax.to($bigBall, 0.4, {
+        x: e.pageX - 15,
+        y: e.pageY - 15,
+      });
+      TweenMax.to($smallBall, 0.1, {
+        x: e.pageX - 5,
+        y: e.pageY - 7,
+      });
+    }
+
+    // Hover an element
+    function onMouseHover(e) {
+      TweenMax.to($bigBall, 0.3, {
+        scale: 4,
+      });
+      e.setAttribute('class', 'hidden')
+    }
+    function onMouseHoverOut(e) {
+      TweenMax.to($bigBall, 0.3, {
+        scale: 1,
+      });
+      e.setAttribute('class', 'appear')
+    }
+    
   };
   window.addEventListener('load', init);
 })(window, document);
