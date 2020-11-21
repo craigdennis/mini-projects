e77f7a482d29ee5a5e24d35d42e4e1e18f4487e7
Unknown <craigdennis1990@gmail.com>
2020-11-21 16:43:54 +0000

    started coding

--- 404-page/index.js
+++ 404-page/index.js
@@ -38,43 +38,16 @@
       createImage(i);
     }
 
-    const $bigBall = document.querySelector('.cursor__ball--big');
-    const $smallBall = document.querySelector('.cursor__ball--small');
-    const $hoverables = document.querySelectorAll('.hoverable');
+    function update(e) {
+      var x = e.clientX || e.touches[0].clientX;
+      var y = e.clientY || e.touches[0].clientY;
 
-    // Listeners
-    document.body.addEventListener('mousemove', onMouseMove);
-    for (let i = 0; i < $hoverables.length; i++) {
-      $hoverables[i].addEventListener('mouseenter', onMouseHover);
-      $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+      document.documentElement.style.setProperty('--cursorX', x + 'px');
+      document.documentElement.style.setProperty('--cursorY', y + 'px');
     }
 
-    // Move the cursor
-    function onMouseMove(e) {
-      TweenMax.to($bigBall, 0.4, {
-        x: e.pageX - 15,
-        y: e.pageY - 15,
-      });
-      TweenMax.to($smallBall, 0.1, {
-        x: e.pageX - 5,
-        y: e.pageY - 7,
-      });
-    }
-
-    // Hover an element
-    function onMouseHover(e) {
-      TweenMax.to($bigBall, 0.3, {
-        scale: 4,
-      });
-      e.setAttribute('class', 'hidden')
-    }
-    function onMouseHoverOut(e) {
-      TweenMax.to($bigBall, 0.3, {
-        scale: 1,
-      });
-      e.setAttribute('class', 'appear')
-    }
-    
+    document.addEventListener('mousemove', update);
+    document.addEventListener('touchmove', update);
   };
   window.addEventListener('load', init);
 })(window, document);
